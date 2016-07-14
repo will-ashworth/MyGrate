94c835e7ab0f784633f8f6903a90deafb1b6bcf5
Will Ashworth <william.ashworth@gravitatedesign.com>
2016-07-13 18:28:00 -0700

    License added

--- mygration.sh
+++ mygration.sh
@@ -8,21 +8,27 @@
 #   |_|  |_|\__, |\_____|_|  \__,_|\__|_|\___/|_| |_|
 #            __/ |
 #           |___/
-###############################################################################
+#
+#
+#   Copyright 2016 William Ashworth
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
 set -e              # exit on command errors (failsafe; so you MUST handle exit codes properly!)
 set -E              # pass trap handlers down to subshells
 set -o pipefail     # capture fail exit codes in piped commands
 #set -x             # execution tracing debug messages
-
-
-
-###############################################################################
-###############################################################################
-###############################################################################
-##########                   ~~ STOP EDITING! ~~                     ##########
-###############################################################################
-###############################################################################
-###############################################################################
 
 # Other variables
 [ "$NOW" ]             ||  NOW=$(date +"%Y%m%d-%H%M%S")
